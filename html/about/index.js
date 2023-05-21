a2da2f015d7cb03dc50eca0c750b6bfb9c08611b
Himao25253 <128271463+Himao25253@users.noreply.github.com>
2023-05-21 11:00:53 +0900

    Delete html/about directory

--- html/about/index.js
+++
@@ -1,9 +0,0 @@
-var hamburger = document.querySelector('.hamburger-menu');
-var nav = document.querySelector('nav');
-
-hamburger.addEventListener('click', function() {
-  hamburger.classList.toggle('active');
-  if (nav) {
-    nav.classList.toggle('active');
-  }
-});
